62b8486bb824cc6528817698ab9482194f4e1f5a
SahilRK <sahilkulkarni.bca@gmail.com>
2014-02-04 18:25:21 +0530

    Created a basic user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleRailsApp::Application.config.secret_key_base = 'e36a708da58d14305668d2e4ba15269a1a5e9f71952ad581fb9a6fbb4c72b2ef4607cd2ca419132ddf6f1887f10fd6a8dfba2a319a8dd5cc0962d2cfc08cfc53'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleRailsApp::Application.config.session_store :cookie_store, key: '_simple_rails_app_session'
